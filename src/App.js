82d073c36b4cc5893f4904fa74f44a6eb8bad68c
Luis <luissvallaa@gmail.com>
2021-06-30 13:55:40 +0800

    first init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SearchBar from "./SearchBar";
+import AddItem from "./AddItem";
+import { useState } from "react";
+import ItemsDisplay from "./ItemsDisplay";
 
 function App() {
+  const [filters, setFilters] = useState({});
+  const [data, setData] = useState({ items: [] });
+
+  const updateFilters = (searchParams) => {
+    setFilters(searchParams);
+  };
+
+  const addItemToData = (item) => {
+    let items = data["items"];
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(item),
+    };
+    fetch("http://localhost:3000/items", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        items.push(data);
+        setData({ items: items });
+      });
+  };
+
+  const filterData = (data) => {
+    const filteredData = [];
+
+    if (!filters.name) {
+      return data;
+    }
+
+    for (const item of data) {
+      if (filters.name !== "" && item.name !== filters.name) {
+        continue;
+      }
+
+      if (filters.price !== 0 && item.price > filters.price) {
+        continue;
+      }
+
+      if (filters.type !== "" && item.type !== filters.type) {
+        continue;
+      }
+
+      if (filters.brand !== "" && item.brand !== filters.brand) {
+        continue;
+      }
+
+      filteredData.push(item);
+    }
+
+    return filteredData;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row mt-3">
+        <ItemsDisplay items={filterData(data["items"])} />
+      </div>
+      <div className="row mt-3">
+        <SearchBar updateSearchParams={updateFilters} />
+      </div>
+      <div className="row mt-3">
+        <AddItem addItem={addItemToData} />
+      </div>
     </div>
   );
 }
